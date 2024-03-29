90c1651606b7aaac031dbb6b03fd1c5ce1d4fc39
HKLeeeee <dlapdlf1739@gmail.com>
2024-03-29 17:18:19 +0900

    [Feat] husky git hook 설정
    pre-commit : lint
    pre-push : test

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,17 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {ConfigModule} from "@nestjs/config";
-import * as process from "process";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
 
 @Module({
-    imports: [ConfigModule.forRoot({
-        envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.prod",
-        isGlobal: true,
-    }),],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+      isGlobal: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
