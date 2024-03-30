b6ff115789ed9d260afa65f3a3650549e1baf07e
HKLeeeee <dlapdlf1739@gmail.com>
2024-03-31 00:47:11 +0900

    [Feat] 요청 DTO 데이터 유효성 검사

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ forbidNonWhitelisted: true }));
-  const port = configService.get<number>('PORT');
+  const port = configService.get<number>('PORT', 3000);
   await app.listen(port);
 }
 bootstrap();
--- server/src/regular-alarm/dto/request/enroll.request.dto.ts
+++ server/src/regular-alarm/dto/request/enroll.request.dto.ts
@@ -1,15 +1,25 @@
-import { IsNumber, IsString, MaxLength, MinLength } from 'class-validator';
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsNumber,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class EnrollRequestDto {
   @IsString()
   deviceToken: string;
 
   @IsString()
+  @MinLength(4, { message: '시간을 4자리로 입력해주세요.' })
+  @MaxLength(4, { message: '시간을 4자리로 입력해주세요.' })
   time: string;
 
-  @MaxLength(7, { each: true })
-  @MinLength(1)
-  day: number[];
+  @IsNumber({}, { each: true, message: '요일을 숫자로 나타내주세요' })
+  @ArrayMinSize(1, { message: '알람받을 요일이 하루 이상이어야합니다.' })
+  @ArrayMaxSize(7, { message: '알람받을 요일이 7개 초과일 수 없습니다.' })
+  activeDay: number[];
 
   @IsNumber()
   busRouteId: number;
