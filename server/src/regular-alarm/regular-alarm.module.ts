fc27d48cfc5352fc8288e6069d53db8bbf4486da
HKLeeeee <dlapdlf1739@gmail.com>
2024-04-01 02:58:32 +0900

    [Feat] 버스 도착 정보 조회

--- server/src/regular-alarm/regular-alarm.module.ts
+++ server/src/regular-alarm/regular-alarm.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { RegularAlarm, RegularAlarmSchema } from './regular-alarm.schema';
 import { RegularAlarmController } from './regular-alarm.controller';
 import { RegularAlarmService } from './regular-alarm.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: RegularAlarm.name, schema: RegularAlarmSchema },
     ]),
+    ConfigModule,
   ],
   controllers: [RegularAlarmController],
   providers: [RegularAlarmService],
--- server/src/regular-alarm/regular-alarm.service.ts
+++ server/src/regular-alarm/regular-alarm.service.ts
@@ -5,13 +5,21 @@ import { RegularAlarm } from './regular-alarm.schema';
 import { EnrollRequestDto } from './dto/request/enroll.request.dto';
 import { find } from 'rxjs';
 import { MESSAGE } from '../../common/message';
+import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class RegularAlarmService {
+  private readonly serviceKey;
+  private readonly apiUrl;
   constructor(
     @InjectModel(RegularAlarm.name)
     private regularAlarmModel: Model<RegularAlarm>,
-  ) {}
+    private configService: ConfigService,
+  ) {
+    this.serviceKey = this.configService.get<string>('SERVICE_KEY');
+    this.apiUrl = this.configService.get<string>('BUS_INFO_API');
+  }
 
   async enrollAlarm(enrollRegularDto: EnrollRequestDto): Promise<RegularAlarm> {
     const newAlarm = new this.regularAlarmModel(enrollRegularDto);
@@ -31,5 +39,16 @@ export class RegularAlarmService {
 
   async getAll(): Promise<RegularAlarm[]> {
     return this.regularAlarmModel.find();
+  }
+
+  async busArriveInfo(busRouteId: number, arsId: number): Promise<string> {
+    const requestUrl = `${this.apiUrl}?ServiceKey=${this.serviceKey}&arsId=${arsId}&resultType=json`;
+    const result = (await axios.get(requestUrl)).data;
+    if (result.msgHeader.headerCd !== '0') {
+      throw new Error('버스 도착 정보 조회 실패');
+    }
+    const infoList = result.msgBody.itemList;
+    const info = infoList.filter((each) => each.busRouteId == busRouteId);
+    return info[0].arrmsg1;
   }
 }
