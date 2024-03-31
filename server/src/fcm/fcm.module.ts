f70746e91032fb0614571e22668983267f8707d4
HKLeeeee <dlapdlf1739@gmail.com>
2024-03-31 21:25:28 +0900

    [Feat] Firebase admin sdk 설정

---
+++ server/src/fcm/fcm.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { FcmService } from './fcm.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { FcmController } from './fcm.controller';
+import * as admin from 'firebase-admin';
+
+const FirebaseProvider = {
+  provide: 'FIREBASE_APP',
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => {
+    return admin.initializeApp({
+      credential: admin.credential.cert(
+        configService.get<string>('FCM_CONFIG_PATH'),
+      ),
+    });
+  },
+};
+
+@Module({
+  providers: [FcmService, FirebaseProvider],
+  controllers: [FcmController],
+})
+export class FcmModule {}
