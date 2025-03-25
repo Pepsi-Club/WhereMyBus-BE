fb8d7c636ffc25cf5747e3e3ba4a4f2548e22a8b
HKLeeeee <dlapdlf1739@gmail.com>
2025-03-25 23:36:38 +0900

    [Feat] minVer 제공 API

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -2,6 +2,10 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import * as process from 'process';
 
+type VersionObject = {
+  ver: string;
+};
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -9,5 +13,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('minVer')
+  minVer(): VersionObject {
+    return { ver: '1.2.5' };
   }
 }
